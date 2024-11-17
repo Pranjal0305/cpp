2046d4105d5dc61e70af58cfb7b364d1a0acb2a7
Pranjal Rai <pranjalrai230634@acropolis.in>
2024-11-17 20:35:43 +0530

    Update bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include <string>
+using namespace std;
+class Employee
+{ 
+    private:
+    int id;
+    string name;
+    float fees;
+    public: 
+    Employee(int i, string n)
+    { 
+        this->id = i;
+        this->name = n;
 
+    }
+
+    Employee(int i, string n, float f):
+        Employee(id,name)
+    {
+        this->fees = f;
+    }
+
+    void display()
+    {
+        cout<<"id: "<<id<<" name: "<<name<<" fees: "<<fees<<endl;
+    }
+};
+
+int main()
+{ 
+    Employee e1(1,"amit");
+    Employee e2(2,"sumit",2000);
+    e1.display();
+    e2.display();
+
+    
+    return 0;
+}
